ed4f3dc13794638fde81b801ceb7170c9d34f6ec
EmirErtorer <emir1.ertorer@gmail.com>
2024-05-23 01:26:45 +0300

    Implemented the system call

--- Kernel Space/set_lottery_scheduler.c
+++ Kernel Space/set_lottery_scheduler.c
@@ -1,9 +1,14 @@
 // Kernel Space System Call Function Code: This will go to  /usr/src/kernel-source-2.4.27/fs
 
 #include <linux/set_lottery_ticket.h>
-#include <sched.c> // global variable sched_lottery is defined in sched.c
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+
+extern int sched_lottery; // Declare the global variable
 
 asmlinkage int sys_set_lottery_scheduler(int enabled){
 	// Implement the logic here
 	sched_lottery = enabled;
+	return 0;
  }
